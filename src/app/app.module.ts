2c4482976e5a3e8dc990b5ff7f1bce156b659240
Julian Ospina <julian.ospina83@gmail.com>
2024-02-10 21:02:11 -0500

    division de modulos y mas bases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CounterModule
+    CounterModule,
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
